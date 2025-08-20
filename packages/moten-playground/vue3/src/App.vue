a635d83677b03b7231fd525b2fbd520e919f3ffb
xiaotaiyang123408 <3245412653@qq.com>
2025-08-20 13:11:01 +0800

    fix: 🐛 BUG修复
    组件版本切换BUG修复

--- packages/moten-playground/vue3/src/App.vue
+++ packages/moten-playground/vue3/src/App.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
-import moten from '@moten/ui'
-import '@moten/ui/style'
-const { MoImage } = moten
+import moten from "@moten/ui";
+import "@moten/ui/style";
+const { MoImage } = moten;
 </script>
 
 <template>
   <div>
-    <div>123</div>
-    <MoImage src="https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"></MoImage>
-
+    <MoImage
+      src="https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    ></MoImage>
   </div>
-  </template>
+</template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
